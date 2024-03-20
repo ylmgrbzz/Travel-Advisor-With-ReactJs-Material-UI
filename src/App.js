d80a29bd60bba9c543960d725ec6dbabe2f54c52
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-20 21:15:39 +0300

    Refactor App.js and Header.jsx components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div className="App">Learn React</div>;
-}
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+// import { getPlacesData, getWeatherData } from "./api/travelAdvisorAPI";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header
+      // onPlaceChanged={onPlaceChanged} onLoad={onLoad}
+      />
+    </>
+  );
+};
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = (props) => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <div
+          style={{
+            display: "flex",
+            padding: "10px",
+          }}
+        >
+          <Typography variant="h6">Explore new places</Typography>
+          <Autocomplete
+            onLoad={props.onLoad}
+            onPlaceChanged={props.onPlaceChanged}
+          >
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+          </Autocomplete>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
